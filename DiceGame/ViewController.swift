d903e0ce1f5847bbf46696566246ea5cfe5db639
tireday24 <skansmfqh7312@gmail.com>
2022-05-25 13:43:29 +0900

    Git Setting Xcode project file

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    var diceArray : [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+                                 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        
+        // 첫번째 이미지 뷰와 2번째 이미지 뷰를 랜덤으로 변경
+        firstImageView.image = diceArray.randomElement()
+        
+        secondImageView.image = diceArray.randomElement()
+        
+    }
+
+    
+    
+    
 }
 
